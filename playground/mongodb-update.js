9db0053076814846794f02c6374c78b4a227c9fc
Peter <ivanponikve@gmail.com>
2018-11-25 13:02:25 +0100

    API init

---
+++ playground/mongodb-update.js
@@ -0,0 +1,24 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  const db = client.db('TodoApp');
+
+
+db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID("5bf53fc267b58733d196c245")
+}, {
+    $set: {
+        text: 'updated lunch'
+    }
+}).then((results) => {
+    console.log(JSON.stringify(results, undefined, 2));
+})
+
+  // db.close();
+});
