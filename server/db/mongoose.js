05efd658242192ffdde02bcbd35815964f697417
petar <piterjov@gmail.com>
2018-12-09 13:52:43 +0100

    fixing bug with requiring mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,4 @@
-var mongoose = require('server/db/mongoose');
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://petar:ratep128@ds263137.mlab.com:63137/todoapp');
